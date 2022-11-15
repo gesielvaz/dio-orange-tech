a4de19e24c040d81063c8a9e1f191ce7f9bcf673
GesielVaz <gesieldosanjosvaz@gmail.com>
2022-11-14 22:55:16 -0300

    init aula1

---
+++ 01-variaveis e condicionais/aula.js
@@ -0,0 +1,2 @@
+let valor = 10 /10;
+console.log(valor);
---
+++ 02- condicionais/aula.js
@@ -0,0 +1,12 @@
+const numero = 0;
+
+
+const numeropar = (numero % 2) === 0;
+if (numeropar) {
+    console.log("sim");
+    return
+}
+if (numeropar !== 0){
+    console.log("não")
+    return
+}
